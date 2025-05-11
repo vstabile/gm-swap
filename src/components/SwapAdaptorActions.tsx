a9441b5b17eaf204a3d3923c869f7b76bc65ff95
Victor Stabile <vmstabile@gmail.com>
2025-05-11 08:28:29 -0300

    improved adaptor signature support information

--- src/components/SwapAdaptorActions.tsx
+++ src/components/SwapAdaptorActions.tsx
@@ -1,20 +1,35 @@
 import { LucideHourglass, LucideLoader } from "lucide-solid";
-import { createSignal, Match, Show, Switch } from "solid-js";
+import { createMemo, createSignal, Match, Show, Switch } from "solid-js";
 import { GenerateAdaptors } from "~/actions/generateAdaptors";
 import { actions } from "~/actions/hub";
 import { Swap } from "~/queries/swap";
 import { SignTakenEvent } from "~/actions/signTakenEvent";
 import { useAuth } from "~/contexts/authContext";
 import { RevokeProposal } from "~/actions/revokeProposal";
-import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
 
 export default function SwapAdaptorActions(props: { swap: Swap }) {
   const { state } = useAuth();
   const [isSendingAdaptor, setIsSendingAdaptor] = createSignal(false);
   const [isPublishing, setIsPublishing] = createSignal(false);
   const [isRevoking, setIsRevoking] = createSignal(false);
+  const [isAdaptorSupportDialogOpen, setIsAdaptorSupportDialogOpen] =
+    createSignal(false);
+
+  const isAdaptorDisabled = createMemo(() => {
+    return isSendingAdaptor() || isRevoking() || state.method !== "nsec";
+  });
 
   async function sendAdaptor() {
+    if (isAdaptorDisabled()) return setIsAdaptorSupportDialogOpen(true);
+
     setIsSendingAdaptor(true);
 
     try {
@@ -75,24 +90,40 @@ export default function SwapAdaptorActions(props: { swap: Swap }) {
           </div>
         </Match>
         <Match when={props.swap.state === "adaptor-pending"}>
-          <Tooltip>
-            <TooltipTrigger
-              class="flex items-center justify-center border-green-700 bg-gradient-to-br from-green-600 to-green-500 drop-shadow hover:from-green-600 hover:to-green-400 disabled:from-green-600/60 disabled:to-green-600/60 text-white px-2 py-1 rounded-md w-full"
-              onClick={sendAdaptor}
-              disabled={
-                isSendingAdaptor() || isRevoking() || state.method !== "nsec"
-              }
-            >
-              <Show when={isSendingAdaptor()} fallback="Send Adaptor Signature">
-                <LucideLoader class="w-3 h-3 mr-1 animate-spin" /> Sending
-              </Show>
-            </TooltipTrigger>
-            <TooltipContent>
-              Sign in with an nsec to generate the adaptor signature.
-            </TooltipContent>
-          </Tooltip>
           <button
-            class="flex  items-center justify-center border border-red-600 text-red-600 px-2 py-1 rounded-md w-full hover:bg-red-600 hover:text-white disabled:bg-red-600/80 disabled:text-white"
+            class={
+              (isAdaptorDisabled()
+                ? "bg-green-600/60 cursor-default"
+                : "bg-gradient-to-br from-green-600 to-green-500 drop-shadow hover:from-green-600 hover:to-green-400") +
+              " flex items-center justify-center border-green-700 text-white px-2 py-1 rounded-md w-full"
+            }
+            onClick={sendAdaptor}
+          >
+            <Show when={isSendingAdaptor()} fallback="Send Adaptor Signature">
+              <LucideLoader class="w-3 h-3 mr-1 animate-spin" /> Sending
+            </Show>
+          </button>
+          <Dialog
+            open={isAdaptorSupportDialogOpen()}
+            onOpenChange={setIsAdaptorSupportDialogOpen}
+          >
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle class="pb-2">
+                  Generating Adaptor Signatures is not Supported
+                </DialogTitle>
+                <DialogDescription>
+                  NIP-07 extensions and NIP-46 remote signers still do not
+                  support generating adaptor signatures. For now, you need to
+                  sign in with an nsec, so that the client code can perform the
+                  calculations needed. Note that only one party of the swap
+                  needs to do this, by default the one who created the proposal.
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+          <button
+            class="flex items-center justify-center border border-red-600 text-red-600 px-2 py-1 rounded-md w-full hover:bg-red-600 hover:text-white disabled:bg-red-600/80 disabled:text-white"
             onClick={handleRevokeProposal}
             disabled={isRevoking() || isSendingAdaptor()}
           >
